38f24e5c738dd5155c539a059c38f7febef5bbad
thojea <thomas.jeanmonod@gmail.com>
2015-11-03 09:24:47 +0100

    add form to the naviagtion menu add message "no book find" on the search page update template (add xhtml workspace)

--- src/main/java/org/books/presentation/CatalogBean.java
+++ src/main/java/org/books/presentation/CatalogBean.java
@@ -28,19 +28,26 @@ public class CatalogBean implements Serializable {
 
     private String searchString;
 
+    /**
+     * Use this methode to find books if it doesn't find book, it send a message
+     * to the user
+     *
+     * @return null - Stay on the same page<s
+     */
     public String findBooks() {
         this.booksList = bookstore.searchBooks(searchString);
 
         try {
             Book b = bookstore.findBook(searchString);
-            if(!this.booksList.contains(b)){
-               this.booksList.add(b); 
+            if (!this.booksList.contains(b)) {
+                this.booksList.add(b);
             }
-            
+
         } catch (BookNotFoundException e) {
             //Nothing
         }
-        if(booksList.isEmpty()){
+        //Send a message if the list is empty
+        if (booksList.isEmpty()) {
             FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_INFO, "No books found", "");
             FacesContext context = FacesContext.getCurrentInstance();
             context.addMessage(null, fm);
@@ -48,9 +55,62 @@ public class CatalogBean implements Serializable {
         return null;
     }
 
+    /**
+     * Use this methode to go on the details page
+     *
+     * @param b selectedBook
+     * @return the details page
+     */
     public String setDetail(Book b) {
         this.selectedBook = b;
         return "bookDetails?faces-redirect=true";
+    }
+
+    /**
+     * Use this methode to show the next book
+     *
+     * @return go on the details page
+     */
+    public String nextBook() {
+        int indexBook = booksList.indexOf(selectedBook);
+        try {
+            return setDetail(booksList.get(indexBook + 1));
+        } catch (IndexOutOfBoundsException e) {
+            //Nothing
+            return null;
+        }
+    }
+
+    /**
+     * Use this methode to show the previous book
+     *
+     * @return go on the details page
+     */
+    public String previousBook() {
+        int indexBook = booksList.indexOf(selectedBook);
+        try {
+            return setDetail(booksList.get(indexBook - 1));
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @return true if the selected book isn't the last element of the booklist
+     */
+    public boolean hasNext() {
+        int indexBook = booksList.indexOf(selectedBook);
+        return indexBook < booksList.size() - 1;
+    }
+
+    /**
+     *
+     * @return true if the selected book isn't the first element of the booklist
+     */
+    public boolean hasPrevious() {
+        int indexBook = booksList.indexOf(selectedBook);
+        return indexBook > 0;
     }
 
     public Book getselectedBook() {
