5df994c07df6e9ed910aac99f129338cc025bab0
thojea <thomas.jeanmonod@gmail.com>
2015-11-12 10:33:12 +0100

    Add super components to xhmtl page

--- src/main/java/org/books/presentation/bean/CustomerBean.java
+++ src/main/java/org/books/presentation/bean/CustomerBean.java
@@ -63,6 +63,14 @@ public class CustomerBean implements Serializable {
         return null;
     }
     
+    public String goOnPageCustomerDetails(){
+        return "/user/customerDetails?faces-redirect=true&menuId=3";
+    }
+    
+    public String goOnPageChangePassword(){
+        return "/user/changePassword?faces-redirect=true&menuId=3";
+    }
+    
     public Type[] getCreditCardTypes() {
         return Type.values();
     }
