93014df105db49ad8c7dfac2d15d46b9ce28f9a7
thojea <thomas.jeanmonod@gmail.com>
2015-11-12 11:26:46 +0100

    Add ajax to compenent accountInfo

---
+++ src/main/java/org/books/presentation/validator/EMailChangeValidator.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.books.presentation.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+import org.books.application.Bookstore;
+import org.books.application.BookstoreException;
+import org.books.util.MessageFactory;
+
+/**
+ *
+ * @author tjd
+ */
+@FacesValidator(EMailChangeValidator.VALIDATOR_ID)
+public class EMailChangeValidator implements Validator {
+
+    public static final String VALIDATOR_ID = "org.books.presentation.validator.emailChangevalidator";
+
+    public static final String USER_EXIST = "org.books.presentation.validator.emailChangevalidator.USER_EXIST";
+
+    @Inject
+    private Bookstore bookstore;
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        try {
+            bookstore.findCustomer((String) value);
+            //User find --> throw Exception
+            FacesMessage facesMessage = MessageFactory.getMessage(FacesMessage.SEVERITY_ERROR, USER_EXIST);
+            throw new ValidatorException(facesMessage);
+        } catch (BookstoreException ex) {
+            //No user found --> OK !
+        }
+    }
+
+}
