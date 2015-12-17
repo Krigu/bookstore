3369d7c261ab4b220e44f3a006c15d846a449065
thojea <thomas.jeanmonod@gmail.com>
2015-12-17 13:47:59 +0100

    add customer example to bookstore_data.xml implement find method in dao update customer query implement customerquerytest

--- bookstore-jpa/src/main/java/org/books/data/dao/CustomerDAOBean.java
+++ bookstore-jpa/src/main/java/org/books/data/dao/CustomerDAOBean.java
@@ -4,7 +4,12 @@ import org.books.data.dao.generic.GenericDAOImpl;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.TypedQuery;
 import org.apache.log4j.Logger;
+import org.books.data.dto.CustomerInfo;
+import org.books.data.entity.Book;
 import org.books.data.entity.Customer;
 
 /**
@@ -15,26 +20,34 @@ import org.books.data.entity.Customer;
  *
  * @author Thomas Jeanmonod
  *
- **/
+ *
+ */
 @Stateless
 public class CustomerDAOBean extends GenericDAOImpl<Customer> implements CustomerDAOLocal {
 
     private static final Logger LOGGER = Logger.getLogger(CustomerDAOBean.class);
-    
-    public CustomerDAOBean(){
+
+    public CustomerDAOBean() {
         super(Customer.class);
     }
-   
 
     @Override
     public Customer find(String email) throws EntityNotFoundException {
-        //TODO
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<Customer> query = entityManager.createNamedQuery(Customer.FIND_BY_EMAIL, Customer.class);
+        query.setParameter("email", email);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException | NonUniqueResultException e) {
+            throw new EntityNotFoundException(e.getMessage());
+        }
     }
 
     @Override
     public List search(String name) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        TypedQuery<CustomerInfo> query = entityManager.createNamedQuery(Customer.FIND_BY_NAME, CustomerInfo.class);
+        query.setParameter("firstName", name);
+        query.setParameter("lastName", name);
+        return query.getResultList();
     }
-    
+
 }
